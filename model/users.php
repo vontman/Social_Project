7a06b946a02b9829e1ecdbf4cd2acd295ce1708e
vontman <vontman@gmail.com>
2014-09-20 20:59:18 +0300

    Added view_user function !!!

--- model/users.php
+++ model/users.php
@@ -1,23 +1,27 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of users
- *
- * @author omar
- */
 class users {
     public $link;
     public $functions;
-    public function __construct() {
+    public function __construct(){
         include_once './connect.php';
         include_once './function.php';
         $this->functions=new db_functions;
         $this->functions->table_name='users';
+    }
+    public function view_user($user_id){
+        $cols=array("id","username","email","firstname","lastname","mobile_number","job","gender","country_id","image","cover","created");
+        try{
+            $user=$this->functions->select($cols, $user_id);
+            return $user;
+        }
+        catch (Exception $ex){
+            return mysqli_errno($this->link);
+        }
+    }
+    public function check_login($username,$password){
+        $user['username']=$username;
+        $user['password']=$password;
+        $this->functions->select($user);
     }
 }
