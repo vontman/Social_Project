48c3728469789d17e144f5c09a881ab778b94079
vontman <vontman@gmail.com>
2014-09-21 00:11:33 +0300

    another Fixings!!

--- model/functions-test.php
+++ model/functions-test.php
@@ -8,4 +8,13 @@ $functions->table_name='users';
 //$input['password']=md5('test');
 //$input['email']='hamada@rmial.com';
 //$functions->insert($input);
+////end insert test !!
 //print_r($functions->select(array('id','username','email'),false,array('id','DESC'),array(1=>3)));
+////end select test!!
+//$functions->delete(4);
+////end delete test!!!
+//$update['username']='ellol';
+//$update['email']='test@lol.com';
+//$update['firstname']='elawy';
+//$functions->update($update,2);
+////end update test!!
