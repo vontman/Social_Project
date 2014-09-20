77114c911b889aa5cd34dcb8268e1bccb5a820d5
vontman <vontman@gmail.com>
2014-09-21 02:54:38 +0300

    test on new function !!

--- model/functions-test.php
+++ model/functions-test.php
@@ -50,4 +50,5 @@ $friends=new friends();
 //echo $friends->accept(1, true);
 //echo $friends->accept(2, false);
 //end accepted test !!!
-print_r($friends->view_friends(2));
+//print_r($friends->view_friends(2));
+//end view friends test !!
