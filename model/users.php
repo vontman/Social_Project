0aea9393cc9943c1d534c72c3b73cead60c04830
vontman <vontman@gmail.com>
2014-09-20 23:02:37 +0300

    Added new login function to users !!

--- model/users.php
+++ model/users.php
@@ -8,11 +8,15 @@ class users {
         include_once './function.php';
         $this->functions=new db_functions;
         $this->functions->table_name='users';
+        $this->link=$this->functions->link;
     }
-    public function adduser($arry){
-        $this->functions->insert();
-        $id=$arry;
-        return $id;
+    public function adduser($user){
+        try{
+            $user_id=$this->functions->insert($user);
+            return $user_id;
+        } catch (Exception $ex) {
+            return mysqli_errno($this->link);
+        }
     }
     public function check_username($username){
         $cols[]='id'; 
@@ -39,12 +43,42 @@ class users {
             return mysqli_errno($this->link);
         }
     }
-    public function check_login($username,$password){
-        $user['username']=$username;
-        $user['password']=$password;
+    public function login($username,$password){   // Function To login and return User Id !!
+//        try{
+//            $query="SELECT id,username FROM users WHERE username='$username'";
+//            $sql=  mysqli_query($this->link, $query);
+//            if(mysqli_affected_rows($this->link)>0){    
+//                $query="SELECT id,password FROM users WHERE password='$password'";
+//                $sql=  mysqli_query($this->link, $query);
+//                if(mysqli_affected_rows($this->link)>0){
+//                    return 0;
+//                }
+//                else{
+//                    return 2;
+//                }
+//            }
+//            else{
+//                return 1;
+//            }
+//        } catch (Exception $ex) {
+//            return mysqli_errno($this->link);
+//        }
+        $query="SELECT id,username,password FROM uesrs WHERE username='$username' AND password='$password'";
         try{
-            $aff_rows=$this->functions->select($user);
-            return $aff_rows;
+            $sql=  mysqli_query($this->link, $query);
+            $aff_rows=  mysqli_affected_rows($this->link);
+            if($aff_rows>0){
+                while($rows= mysqli_fetch_array($sql)){
+                    $user_id=$rows['id'];
+                }
+                $user_login['logged_in']='1';
+                $user_login['last_logged']=date("dd-mm-yy");
+                $this->functions->update($user_login, $user_id);
+                return $user_id;
+            }
+            else{
+                return $aff_rows;
+            }
         } catch (Exception $ex) {
             return mysqli_errno($this->link);
         }
