4434a6dc25767c52f32cad1274aaa1e75bb1d586
vontman <vontman@gmail.com>
2014-09-21 02:14:32 +0300

    added friend_check function
    to avoid adding an existing friend !!

--- model/friends.php
+++ model/friends.php
@@ -12,16 +12,39 @@ class friends {
         $this->link=$this->functions->link;
         $this->date=date('Y/m/d h:i:s', time());
     }
-    public function add_friend($user_id,$friend_id){
-        $add_friend['user_id']=$user_id;
-        $add_friend['friend_id']=$friend_id;
-        $add_friend['accepted']=0;
-        $add_friend['created']=$this->date;
+    public function check_friends($user_id,$friend_id){
+        $query="SELECT id,user_id,friend_id FROM friends WHERE user_id='$user_id' AND friend_id='$friend_id'";
         try{
-            $this->functions->insert($add_friend);
+            $sql=  mysqli_query($this->link, $query);
+            $aff_rows=  mysqli_affected_rows($this->link);
+            return $aff_rows;
         } catch (Exception $ex) {
             return mysqli_errno($this->link);
         }
+    }
+    public function add_friend($user_id,$friend_id){
+        $query="SELECT id,user_id,friend_id FROM friends WHERE user_id='$user_id' AND friend_id='$friend_id'";
+        try{
+            $sql=  mysqli_query($this->link, $query);
+            $aff_rows=  mysqli_affected_rows($this->link);
+            if($aff_rows<1){
+                $add_friend['user_id']=$user_id;
+                $add_friend['friend_id']=$friend_id;
+                $add_friend['accepted']=0;
+                $add_friend['created']=$this->date;
+                try{
+                    return $this->functions->insert($add_friend);
+                } catch (Exception $ex) {
+                    return mysqli_errno($this->link);
+                }
+            }
+            else{
+                die("ERROR !!");
+            }
+        } catch (Exception $ex) {
+            return mysqli_errno($this->link);
+        }
+
     }
     public function accept($friend_row,$accept){
         if($accept){
