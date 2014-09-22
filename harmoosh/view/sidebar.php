18420dde6d4e1c04847e7af025963d3a45c8d8da
vontman <vontman@gmail.com>
2014-09-22 23:34:49 +0300

    added new calender

--- harmoosh/view/sidebar.php
+++ harmoosh/view/sidebar.php
@@ -71,7 +71,7 @@
             <li>Events</li>
             <li>Photos</li>
             <!--<iframe class="cal" src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=200&amp;wkst=1&amp;bgcolor=%23333333&amp;src=%23contacts%40group.v.calendar.google.com&amp;color=%23AB8B00&amp;ctz=Africa%2FCairo" style=" border-width:0; background-color: #0c383e; " width="190" height="200" frameborder="0" scrolling="no" margin-top="10px "></iframe>-->
-            <div class='calender'>dsklajkl</div>
+            <div class='calender'></div>
         </ul>
     
         
