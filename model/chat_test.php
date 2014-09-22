267a8a67d4166d17671b53f5364bad83da62db41
vontman <vontman@gmail.com>
2014-09-22 14:48:26 +0300

    added chat box in chattest.php !!!

--- model/chat_test.php
+++ model/chat_test.php
@@ -32,8 +32,24 @@
                     }
             });
         });
+        var scroll=$('.msgs').height();
+        $('.messages').scrollTop(scroll);
     });
 </script>
+<style>
+    .msgs{
+        padding:10px 5px;
+        
+    }
+    .msgs div{
+        max-width: 95%;
+        background:red;
+        margin:5px 2px;
+        padding:5px 2px;
+        line-height: 100%;
+        bottom:0;
+    }
+</style>
 <div style="width:45%;float:left;">
     <input name='user' class="user" align='left' type=text placeholder="send" >
     <label class="user_submit">user</label><br>
@@ -52,4 +68,133 @@
     <label class="send">send</label><br>
     <textarea class="view">message</textarea><br>
     <label class="done">!</label>
+</div>
+<div class="chat" style="overflow-x: hidden;overflow-y: hidden;width:240px;height:240px;background:lightgray;">
+    <div class="messages" style="height:215px;overflow-x:hidden;overflow-y: auto;">
+        <div class="msgs" style="width:95%;overflow-x: hidden;overflow-y: auto;background:yellow;">
+            <div>
+                hamada1
+            </div>
+            <div>
+                hamada2
+            </div>
+            <div>
+                hamada3
+            </div>
+            <div>
+                hamada4
+            </div>
+            <div>
+                hamada1
+            </div>
+            <div>
+                hamada2
+            </div>
+            <div>
+                hamada3
+            </div>
+            <div>
+                hamada4
+            </div>
+            <div>
+                hamada1
+            </div>
+            <div>
+                hamada2
+            </div>
+            <div>
+                hamada3
+            </div>
+            <div>
+                hamada4
+            </div>
+            <div>
+                hamada1
+            </div>
+            <div>
+                hamada2
+            </div>
+            <div>
+                hamada3
+            </div>
+            <div>
+                hamada4
+            </div>
+            <div>
+                hamada1
+            </div>
+            <div>
+                hamada2
+            </div>
+            <div>
+                hamada3
+            </div>
+            <div>
+                hamada4
+            </div>
+            <div>
+                hamada1
+            </div>
+            <div>
+                hamada2
+            </div>
+            <div>
+                hamada3
+            </div>
+            <div>
+                hamada4
+            </div>
+            <div>
+                hamada1
+            </div>
+            <div>
+                hamada2
+            </div>
+            <div>
+                hamada3
+            </div>
+            <div>
+                hamada4
+            </div>
+            <div>
+                hamada1
+            </div>
+            <div>
+                hamada2
+            </div>
+            <div>
+                hamada3
+            </div>
+            <div>
+                hamada4
+            </div>
+            <div>
+                hamada1
+            </div>
+            <div>
+                hamada2
+            </div>
+            <div>
+                hamada3
+            </div>
+            <div>
+                hamada4
+            </div>
+            <div>
+                hamada1
+            </div>
+            <div>
+                hamada2
+            </div>
+            <div>
+                hamada3
+            </div>
+            <div>
+                hamada4
+            </div>
+        </div>
+    </div>
+    <div class="chat_type" style="height:20px;width:100%;">
+        <input type="text" placeholder="message" style="width:85%;"><label style="width:14%;background:violet;font-size:18px;height: 100%;">send</label>
+    </div>
 </div>
