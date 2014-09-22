16c0bbaa404773cbbae1883e3599fe08413d5347
vontman <vontman@gmail.com>
2014-09-23 00:36:47 +0300

    Fixed Calender Style :dancer:

---
+++ view/slider.php
@@ -0,0 +1,54 @@
+        <div id="left_notifications">
+
+            <div class="slider leftslider">
+<!--                <div class="close">
+
+                </div>-->
+                <div>
+                    <ul>
+                        <li>
+                            <image src="user.png"/>
+                            <div class='notification'>
+                                <font>user has commented on your post</font>
+                            </div>
+                            <div id='notification_date'>
+                                3 hours ago..
+                            </div>
+                        </li>
+                        <li>
+                            <image src="user.png" height="50px"/>
+                            <div class='notification'>
+                                <font>user has commented on your post</font>
+                            </div>
+                            <div id='notification_date'>
+                                3 hours ago..
+                            </div>
+                        </li>
+                        <li>
+                            <image src="user.png" height="50px"/>
+                            <div class='notification'>
+                                <font>user has commented on your post</font>
+                            </div>
+                            <div id='notification_date'>
+                                3 hours ago..
+                            </div>
+                        </li>
+                        <li>
+                            <image src="user.png" height="50px"/>
+                            <div class='notification'>
+                                <font>user has commented on your post</font>
+                            </div>
+                            <div id='notification_date'>
+                                3 hours ago..
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="icon">
+                <div class="not_num">
+                    <font>10</font>
+                </div>
+                <img src="not3.png"/>
+            </div>
+        </div>
