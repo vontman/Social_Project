200d0c5d86b3bb78dedea611e25369bcfbe29e56
Eng-AALAA <ahmedalaa.9@gmail.com>
2014-09-24 00:46:21 +0200

    last

--- harmoosh/model/signup.php
+++ harmoosh/model/signup.php
@@ -4,7 +4,7 @@
   if(isset($_GET["username"])){
       $username=@$_GET["username"];
              $users->check_username($username);
-        if( check_username($username)<0){
+        if( $username<0){
            echo "username already exist";                        
          }else{
              echo" available";
@@ -13,7 +13,7 @@
    if(isset($_GET["email"])){
         $email=@$_GET["email"];
 $users->check_email($email);
-   if( check_username($username)<0){
+   if( $email<0){
        echo "email already exist";                        
         }else{
             echo" available";
