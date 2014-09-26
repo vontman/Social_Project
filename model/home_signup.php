7a894105938c6760db1c682986f9bc44b3d7479e
vontman <vontman@gmail.com>
2014-09-26 23:20:08 +0200

    !!!

--- model/home_signup.php
+++ model/home_signup.php
@@ -18,27 +18,25 @@
         }
        }
 if(isset($_POST["username"])&&isset($_POST["email"])&&isset($_POST['password'])){
-    
     $username=@$_POST['username'];
-    $password=@$_POST['password'];
+    $password=@$md5($_POST['password']);
     $email=@$_POST['email'];
-     $array['username']=$username;
-      $array['password']=$password;
-       $array['email']=$email;
+    $array['username']=$username;
+    $array['password']=$password;
+    $array['email']=$email;
        
     $fname=@$_POST['fname'];
     $lname=@$_POST['lname'];
     $date=@$_POST['date'];
     $gender=@$_POST['gender'];
     $num=@$_POST['num'];
-   
-     
+    
        $array['firstname']=$fname;
        $array['lastname']=$lname;
        $array['birthday']=$date;
        $array['gender']=$gender;
        $array['mobile_number']=$num;
-        $location=@$_POST['location'];
+       $location=@$_POST['location'];
        $array['country_id']=$location;
-       $users->adduser($array);
+       echo $users->adduser($array);
 }
