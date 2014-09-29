2c62f5f4c9082fa85cbfd20cd2a843e5700ffd28
Eng-AALAA <ahmedalaa.9@gmail.com>
2014-09-29 17:00:15 +0200

    edit

--- js/message.js
+++ js/message.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
             $('.message_field').css('visibility','hidden'); 
                 $('.message').click(function(){
-    //               $('.m1').css('visibility','visible');
-                   $('.message_field').append('.message_field');
+                   $('.m1').css('visibility','visible');
+    //               $('.message_field').append('.message_field');
 //                   if('.m1'){
 //                       $('.m2').css('visibility','visable');
 //                      $('.message_fields').append('.m2');
