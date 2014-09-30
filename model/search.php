ef4501ffce0b79d0d9ffdb8a83f1451f19d65d23
vontman <vontman@gmail.com>
2014-09-30 02:15:43 +0200

    trying search for users !!!

--- model/search.php
+++ model/search.php
@@ -5,8 +5,17 @@ if(isset($_GET['keywords'])){
     $users=new users();
     $results=$users->search($keywords);
     foreach ($results as $k=>$v){
-        echo $v['id'];
-        echo $v['username'];
+        ?><tr>
+            <td>
+                <img src='../user.png'/>
+            </td>
+            <td>
+                <div>
+                    <a href='?user=<?php echo $v['id'];?>'><?php echo $v['username']; ?></a>
+                </div>
+            </td>
+        </tr>
+            <?php
     }
 }
     
--- model/search_test.php
+++ model/search_test.php
@@ -8,19 +8,25 @@
 //           }
 //       });
         $('#search_result_wrapper').hide();
+        $('#search_input').click(function(){
+           $(this).css('padding','4px 24px');
+           $(this).css('width','240px')
+        });
+        $i=0;
         $('#search_input').keyup(function(){
-            $('#search_result_wrapper ul').children('a').remove();
+            $('#search_result_wrapper table').children().children('tr').remove();
             if($(this).val().length>2){
-                $('#search_result_wrapper').show();
                 var keywords=$(this).val();
-                var newli;
-                var newli_edit;
                $.ajax({url:'search.php',
                    data:{keywords:keywords},
                    success:function(srch_results){
-                            newli='<a href="?user='+srch_results+'"><li>';
-                           newli_edit=newli+srch_results+'</li></a>';
-                            $('#search_result_wrapper ul').append(newli_edit);
+                        if(srch_results){
+                            $('#search_result_wrapper table').append(srch_results);
+                            if($i==0){
+                                $('#search_result_wrapper').slideDown('fast');
+                            }
+                            $i++;
+                        } 
                        }
                    }
                 );
@@ -28,9 +34,68 @@
         });
     });
 </script>
-
-<input type="text" id='search_input' placeholder="Enter keywords .....">
-<div id='search_result_wrapper' style='background:lightgrey;overflow:hidden;'>
-    <ul>
-    </ul>
+<style>
+    #search_input{
+        border-radius:15px; 
+        background:url('../png/search.png');
+        background-size: contain;
+        background-repeat: no-repeat;
+        height:30px;
+        width:30px;
+        transition:all .45s ease;
+        box-shadow: 2px 2px 8px;
+        border-color: #dedff6;
+    }
+    #search_result_wrapper{
+        display: none;
+    }
+    #search_result_wrapper table{
+        width:350px;
+        background:lightgrey;
+        padding: 0;
+        border-radius:4px;
+        box-shadow: 10px 10px 20px black;
+    }
+    #search_result_wrapper *{
+        transition:all .45s ease;
+    }
+    #search_result_wrapper table tr{
+        background:#dedff6;
+        height:48px;
+        display:block;
+        cursor:pointer;
+        overflow:hidden;
+        border-bottom:2px groove rgba(0,0,0,.15);
+    }
+    #search_result_wrapper table tr:hover{
+        background:#6367de;
+    }
+    #search_result_wrapper table tr:hover a{
+        color:whitesmoke;
+    }
+    #search_result_wrapper table td{
+        height:48px;
+    }
+    #search_result_wrapper td a{
+        float:left;
+        display:block;
+        text-decoration: none;
+        color:#000383;
+        font-size:18px;
+    }
+    #search_result_wrapper td div{
+        display:block;
+        min-width:75%;
+        overflow:hidden;
+    }
+    #search_result_wrapper td img{
+        float:left;
+        height:100%;
+    }
+</style>
+<input type="text" id='search_input'>
+<div id='search_result_wrapper'>
+    <table>
+        
+    </table>
 </div>
