da77d9930d8463b5dc4a4783c59e8462ef2e14f0
vontman <vontman@gmail.com>
2014-10-01 13:31:38 +0200

    fixed error when there are no friends !!

--- view/r_sidebar.php
+++ view/r_sidebar.php
@@ -11,31 +11,33 @@
         <?php
             include_once 'model/friends.php';
             $friends=new friends();
-            $user_friends_ids=$friends->view_friends($user_id);
-            foreach($user_friends_ids as $k=>$v){
-                $li_friend=$users->view_user($v)[0]['logged_in'];
-                if($li_friend=='1'){
-                    $user_friends[]=$users->view_user($v)[0];
+            if($friends->view_friends($user_id)){
+                $user_friends_ids=$friends->view_friends($user_id);
+                foreach($user_friends_ids as $k=>$v){
+                    $li_friend=$users->view_user($v)[0]['logged_in'];
+                    if($li_friend=='1'){
+                        $user_friends[]=$users->view_user($v)[0];
+                    }
                 }
-            }
-            foreach($user_friends_ids as $k=>$v){                
-                $li_friend=$users->view_user($v)[0]['logged_in'];
-                if($li_friend=='0'){
-                    $user_friends[]=$users->view_user($v)[0];
+                foreach($user_friends_ids as $k=>$v){                
+                    $li_friend=$users->view_user($v)[0]['logged_in'];
+                    if($li_friend=='0'){
+                        $user_friends[]=$users->view_user($v)[0];
+                    }
                 }
-            }
-            foreach($user_friends as $k=>$v){
-                   ?>
-                <li user='<?php echo $v['id']; ?>' class="message <?php if($v['logged_in']){
-                    echo 'on';
+                foreach($user_friends as $k=>$v){
+                       ?>
+                    <li user='<?php echo $v['id']; ?>' class="message <?php if($v['logged_in']){
+                        echo 'on';
+                    }
+                    ?>">
+                        <img src="user.png"/>
+                        <?php
+                            echo $v['username'];
+                        ?>
+                    </li>
+                        <?php
                 }
-                ?>">
-                    <img src="user.png"/>
-                    <?php
-                        echo $v['username'];
-                    ?>
-                </li>
-                    <?php
             }
         ?>
         </ul>
