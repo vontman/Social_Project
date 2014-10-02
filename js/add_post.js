cf30321f55d18fb02eeb20d82150795ce15b0fca
vontman <vontman@gmail.com>
2014-10-03 01:24:29 +0200

    !!

--- js/add_post.js
+++ js/add_post.js
@@ -1,7 +1,48 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+var add_post_toggle=false;
+function addpost_toggle(){
+        if(!add_post_toggle){
+//            $('.add_post').hide(1000);
+            $('.add_post').css('height','0');
+            $('#add_post_toggle img').css('transform','translateX(-50%) rotateZ(180deg)');
+            add_post_toggle=true;
+        }else{
+//            $('.add_post').show(1000);
+            $('.add_post').css('height','auto');
+            $('#add_post_toggle img').css('transform','translateX(-50%) rotateZ(0deg)');
+            add_post_toggle=false;
+        }
+}
+$(document).ready(function(){
+    $('#add_post_toggle').click(function(){
+        addpost_toggle();
+    });
+    $('#add_post_sbmt').click(function(){
+        var post_length=$('#add_post_body').val().length;
+        var post_body=$('#add_post_body').val();
+        if(post_length<4){
+            alert('The post is too short !');
+        }else{
+            $.ajax({url:'controller/add_post.php',
+                data:{post_body:post_body},
+                success:function(add_post){
+                    if(add_post){
+                        alert('Post has been added !');
+                    }else{
+                        alert('Error !!');
+                    }
+                }
+            });
+        }
+    });
+});
+$(window).scroll(function(){
+    if($(window).scrollTop()>150){
+        add_post_toggle=false;
+        addpost_toggle();
+    }else{
+        $('.add_post').show(1000);
+        add_post_toggle=true;
+        addpost_toggle();
+    }
+});
 
