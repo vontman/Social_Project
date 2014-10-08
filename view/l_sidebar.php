03ad0767f01f5124ba2e44137d35942b45c1da19
vontman <vontman@gmail.com>
2014-10-08 23:03:26 +0200

    made some nice changes in the auto blend :dancer:

--- view/l_sidebar.php
+++ view/l_sidebar.php
@@ -4,19 +4,19 @@
     </head>
     <form method="Get" action="view/profile.php">
     <div class="sidebar l-sidebar">
-        <div class="user">
-            <img src="user.png">
-            <a href="?user"><div name="profile"><h3><?php echo $user['username']; ?></h3></div></a>
+        <div class="sidebar_wrapper">
+            <div class="user">
+                <img src="user.png">
+                <a href="?user"><div name="profile"><h3><?php echo $user['username']; ?></h3></div></a>
+            </div>
+            <ul class="mains">
+                <li>News Feed</li>
+                <li>Messages</li>
+                <li>Events</li>
+                <li>Photos</li>
+                <!--<iframe class="cal" src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=200&amp;wkst=1&amp;bgcolor=%23333333&amp;src=%23contacts%40group.v.calendar.google.com&amp;color=%23AB8B00&amp;ctz=Africa%2FCairo" style=" border-width:0; background-color: #0c383e; " width="190" height="200" frameborder="0" scrolling="no" margin-top="10px "></iframe>-->
+                <div class='calender'></div>
+            </ul>
         </div>
-        <ul class="mains">
-            <li>News Feed</li>
-            <li>Messages</li>
-            <li>Events</li>
-            <li>Photos</li>
-            <!--<iframe class="cal" src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=200&amp;wkst=1&amp;bgcolor=%23333333&amp;src=%23contacts%40group.v.calendar.google.com&amp;color=%23AB8B00&amp;ctz=Africa%2FCairo" style=" border-width:0; background-color: #0c383e; " width="190" height="200" frameborder="0" scrolling="no" margin-top="10px "></iframe>-->
-            <div class='calender'></div>
-        </ul>
-    
-        
     </div>
     </form>
--- view/r_sidebar.php
+++ view/r_sidebar.php
@@ -6,40 +6,42 @@
     <div id="toggle">
         <image class="image" src="png/arrow454.png"/>
     </div>
-    <div class="online">
-        <ul class="user_friends">
-        <?php
-            include_once 'model/friends.php';
-            $friends=new friends();
-            if($friends->view_friends($user_id)){
-                $user_friends_ids=$friends->view_friends($user_id);
-                foreach($user_friends_ids as $k=>$v){
-                    $li_friend=$users->view_user($v)[0]['logged_in'];
-                    if($li_friend=='1'){
-                        $user_friends[]=$users->view_user($v)[0];
+    <div class="sidebar_wrapper">
+        <div class="online">
+            <ul class="user_friends">
+            <?php
+                include_once 'model/friends.php';
+                $friends=new friends();
+                if($friends->view_friends($user_id)){
+                    $user_friends_ids=$friends->view_friends($user_id);
+                    foreach($user_friends_ids as $k=>$v){
+                        $li_friend=$users->view_user($v)[0]['logged_in'];
+                        if($li_friend=='1'){
+                            $user_friends[]=$users->view_user($v)[0];
+                        }
+                    }
+                    foreach($user_friends_ids as $k=>$v){                
+                        $li_friend=$users->view_user($v)[0]['logged_in'];
+                        if($li_friend=='0'){
+                            $user_friends[]=$users->view_user($v)[0];
+                        }
+                    }
+                    foreach($user_friends as $k=>$v){
+                           ?>
+                        <li user='<?php echo $v['id']; ?>' class="message <?php if($v['logged_in']){
+                            echo 'on';
+                        }
+                        ?>">
+                            <img src="user.png"/>
+                            <?php
+                                echo $v['username'];
+                            ?>
+                        </li>
+                            <?php
                     }
                 }
-                foreach($user_friends_ids as $k=>$v){                
-                    $li_friend=$users->view_user($v)[0]['logged_in'];
-                    if($li_friend=='0'){
-                        $user_friends[]=$users->view_user($v)[0];
-                    }
-                }
-                foreach($user_friends as $k=>$v){
-                       ?>
-                    <li user='<?php echo $v['id']; ?>' class="message <?php if($v['logged_in']){
-                        echo 'on';
-                    }
-                    ?>">
-                        <img src="user.png"/>
-                        <?php
-                            echo $v['username'];
-                        ?>
-                    </li>
-                        <?php
-                }
-            }
-        ?>
-        </ul>
+            ?>
+            </ul>
+        </div>
     </div>
 </div>
