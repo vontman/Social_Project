52f0a029fa0efcc03b6c6c98e2bb51dc5db1d548
vontman <vontman@gmail.com>
2014-10-10 21:51:58 +0200

    !!

--- model/posts.php
+++ model/posts.php
@@ -60,7 +60,7 @@ class posts{
         . "FROM posts "
         . "LEFT JOIN friends ON (friends.user_id=$user_id OR friends.friend_id=$user_id) "
         . "LEFT JOIN users ON users.id=posts.user_id "
-        . "WHERE (posts.user_id=friends.user_id OR posts.user_id=friends.friend_id) "
+        . "WHERE ((posts.user_id=friends.user_id) OR (posts.user_id=friends.friend_id)) "
         . "ORDER BY posts.created DESC "
         . "LIMIT $set_no,$items_no";
         try{
