45d13e084bdea4f6493bb9817428d38146c1408f
Eng-AALAA <ahmedalaa.9@gmail.com>
2014-10-10 22:02:52 +0200

    !!

--- model/messages.php
+++ model/messages.php
@@ -8,15 +8,15 @@ class messages {
         include_once 'connect.php';
         include_once 'functions.php';
         $this->functions=new db_functions();
-        $this->functions->table_name='messages';
+        $this->functions->table_name='chat';
         $this->link=$this->functions->link;
         $this->date=date("Y/m/d h:m:s",time());
     }
-    public function send($user_id,$recieved_id,$message_name,$message_body){
-        $input['user_id']=$user_id;
-        $input['recieved_id']=$recieved_id;
-        $input['name']=$message_name;
-        $input['body']=$message_body;
+    public function send($user_id,$recieved_id,$message_name=FALSE,$message_body){
+        $input['from']=$user_id;
+        $input['to']=$recieved_id;
+       // $input['name']=$message_name;
+        $input['message']=$message_body;
         $input['seen']=0;
         $input['created']=$this->date;
         $id=$this->functions->insert($input);
@@ -25,22 +25,22 @@ class messages {
     public function check($user_id){
         $cols[]='id';
         $cols[]='seen';
-        $cols[]='user_id';
-        $row['cols']['recieved_id']=$user_id;
+        $cols[]='to';
+        $row['cols']['from']=$user_id;
         $row['cols']['seen']=0;
         $messages=$this->functions->select($cols, false, array('created'=>'ASC'), false,$row);
         return $messages;
     }
     public function view($user_id,$message_id=false){
         $cols[]='id';
-        $cols[]='user_id';
-        $cols[]='recieved_id';
+        $cols[]='from';
+        $cols[]='to';
         $cols[]='name';
-        $cols[]='body';
+        $cols[]='message';
         $cols[]='created';
         $row=array();
-        $row['cols']['user_id']=$user_id;
-        $row['cols']['recieved_id']=$user_id;
+        $row['cols']['from']=$user_id;
+        $row['cols']['to']=$user_id;
         $row['relation'][]='OR';
         if($message_id){
             $message=$this->functions->select($cols, $message_id, false, false,$row);
@@ -54,14 +54,14 @@ class messages {
     }
     public function check_new($recieved_id){
         $cols[]='id';
-        $cols[]='user_id';
-        $cols[]='recieved_id';
+        $cols[]='from';
+        $cols[]='to';
         $cols[]='name';
-        $cols[]='body';
+        $cols[]='message';
         $cols[]='created';
         $specific_row=array();
         $specific_row['cols']['seen']=0;
-        $specific_row['cols']['recieved_id']=$recieved_id;
+        $specific_row['cols']['to']=$recieved_id;
         try{
             $new_messages=$this->functions->select($cols, false, false, false, $specific_row);
             return $new_messages;
@@ -70,8 +70,8 @@ class messages {
         }
     }
     public function typing($user_id,$recieved_id){
-        $input['user_id']=$user_id;
-        $input['recieved_id']=$recieved_id;
+        $input['from']=$user_id;
+        $input['to']=$recieved_id;
         try{
             $this->functions->insert($input);
         } catch (Exception $ex) {
@@ -79,8 +79,8 @@ class messages {
         }
     }
     public function check_typing($user_id,$recieved_id) {
-        $specific_row['cols']['user_id']=$user_id;
-        $specific_row['cols']['recieved_id']=$recieved_id;
+        $specific_row['cols']['from']=$user_id;
+        $specific_row['cols']['to']=$recieved_id;
         try{
             $typing=$this->functions->select(false, false, FALSE, false, $specific_row);
             return $typing;
