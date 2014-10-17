65f630d883535900988e4649200debcd831f66eb
vontman <vontman@gmail.com>
2014-10-17 02:06:21 +0200

    trying to fix chat recieve msg >_<

--- controller/chat.php
+++ controller/chat.php
@@ -14,35 +14,34 @@
         }else{
             
         }
-    }
-    if(isset($_GET['user2_id'])){
-        $friend_id=$_GET['user2_id'];
-        if(isset($_GET['last_created'])){            
-            $last_msg=$_GET['last_created'];
-            $new_msgs=$messages->check_selected($user_id, $friend_id, $last_msg);
-            if($new_msgs){
-                foreach($new_msgs as $k=>$v){
-                    $messages->seen($new_msgs);
-                    ?>
-                    <div class='msg_contain' created="<?php echo $v['created']; ?>">
-                        <div class='to'>
-                            <img src='user.png'/>
-                            <span>
-                                <?php echo nl2br($v['message']); ?>
-                            </span>
-                        </div>
+    }elseif(isset($_GET['last_msg'])&&isset($_GET['user_id2'])){
+        $friend_id=$_GET['user_id2'];            
+        $last_msg=$_GET['last_msg'];
+        $new_msgs=$messages->check_selected($user_id, $friend_id, $last_msg);
+        if($new_msgs){
+            foreach($new_msgs as $k=>$v){
+                $messages->seen($new_msgs);
+                ?>
+                <div class='msg_contain' created="<?php echo $v['created']; ?>">
+                    <div class='to'>
+                        <img src='user.png'/>
+                        <span>
+                            <?php echo nl2br($v['message']); ?>
+                        </span>
                     </div>
-                    <?php
-                }
-            }elseif($messages->check_typing($friend_id, $user_id)){
-                echo 'lol';
-            }else{
-                echo false;
+                </div>
+                <?php
             }
-        }elseif(isset($_GET['typing'])){
-            echo 'lollll';
-            echo $messages->typing($user_id, $friend_id);
+        }elseif($messages->check_typing($friend_id, $user_id)){
+            echo '10';
         }else{
+            echo false;
+        }
+    }elseif(isset($_GET['user_id2'])){
+        $friend_id=$_GET['user_id2'];      
+        if(isset($_GET['typing'])){
+            $messages->typing($user_id, $friend_id);
+        }elseif(isset($_GET['typing_remove'])){
             $messages->remove_typing($user_id, $friend_id);
         }
     }
